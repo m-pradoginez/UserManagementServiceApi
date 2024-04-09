b279bd723b6d08f4ef2429038122779584d1d4fb
Murillo Prado <murillo.ginez.ext@spms.min-saude.pt>
2024-04-09 20:01:55 -0300

    Created CRUD methods on endpoint

--- UserManagementServiceApi/Models/User.cs
+++ UserManagementServiceApi/Models/User.cs
@@ -2,9 +2,9 @@
 {
     public class User
     {
-        public long Id { get; set; }
-        public string Name { get; set; }
-        public string Birthdate { get; set; }
-        public bool Active { get; set; }
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Birthdate { get; set; }
+        public bool Active { get; set; } = true;
     }
 }
