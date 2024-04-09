a898ecfc2c8eca4c0f71f4f6928a9b4aa7858bd6
Murillo Prado <murillo.ginez.ext@spms.min-saude.pt>
2024-04-09 13:50:23 -0300

    Initial setup for the project

---
+++ UserManagementServiceApi/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace UserManagementServiceApi.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Birthdate { get; set; }
+        public bool Active { get; set; }
+    }
+}
